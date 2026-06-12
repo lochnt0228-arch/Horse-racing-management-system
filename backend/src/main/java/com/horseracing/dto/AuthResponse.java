4fad5e4a60f86568e7943420bb80ab20bc882d57
Loc Huynh <lochnt0228@ut.edu.vn>
2026-06-12 20:12:35 +0700

    feat(backend): create AuthResponse DTO to return JWT and user details

---
+++ backend/src/main/java/com/horseracing/dto/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.horseracing.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String accessToken;
+    
+    private String refreshToken;
+
+    @Builder.Default
+    private String tokenType = "Bearer";
+
+    private String username;
+    
+    private String email;
+    
+    private String role;
+}
